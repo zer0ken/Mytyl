1ad2b6f6dbdb9373d58ce934b95491e2e5df59de
zer0ken <a37592777@gmail.com>
2020-09-11 21:45:10 +0900

    ~ import order

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,3 @@
-from .literal import get_constant, literals, get_path, get_help, get_cog, get_emoji, get_brief, get_check
 from .singleton import singleton
+from .literal import get_constant, literals, get_path, get_help, get_cog, get_emoji, get_brief, get_check
+
